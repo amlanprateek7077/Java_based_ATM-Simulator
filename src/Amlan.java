687502e044f9e57d2e61c9cd95f588b0b212ebc1
Amlan Satapathy <2201109013_cse@pmec.ac.in>
2025-08-31 23:58:33 +0530

    Delete src/Amlan.java

--- src/Amlan.java
+++
@@ -1,9 +0,0 @@
-package com.copany;
-public class Amlan {
-    public static void main(String[] args) {
-
-        for (int i=0; i<5 ; i++ ) {
-            System.out.println("hello amlan");
-        }
-    }
-}
